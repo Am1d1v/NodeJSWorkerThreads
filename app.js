f32da0320e6cecfa578c354a9198972d83514e4d
Am1d1v <dimadiman1adima@gmail.com>
2024-02-13 18:34:34 +0300

    Worker Threads. Blocking Operation

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
+const factorial = require("./factorial");
 
 
-const compute = () => {
-    
+const compute = (array) => {
+
+    // Just random array for higher load
+    const arr = [];
+    for(let i = 0; i < 30000000; i++){
+        arr.push(i * i);
+    }
+
+    // Factorial of every number in array
+    return array.map(element => factorial(element));
 }
 
 const main = () => {
     performance.mark('Start');
 
-
+    const result = [
+        compute([25, 10, 19, 45]),
+        compute([25, 10, 19, 45]),
+        compute([25, 10, 19, 45])
+    ];
+    console.log(result);
 
     performance.mark('End');
     performance.measure('main', 'Start', 'End');
+
+    // Get Performance Measurement
+    console.log(performance.getEntriesByName('main').pop());
 }
 main();
